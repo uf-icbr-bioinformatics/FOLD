b5cd611edc36832349501b91fc8e5d1db50ea6f0
albertoriva <ariva@ufl.edu>
2020-07-23 20:25:05 -0400

    added many options, started run() method

--- fengc.py
+++ fengc.py
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env python
 
 import sys
 import csv
+import os.path
 import subprocess as sp
 
 ## Utilities
@@ -44,3 +45,190 @@ class SequenceManager(object):
             seq = proc.stdout.readline()
             seqs.append(Sequence(name, seq))
         return seqs
+
+# Simple GTF parser
+
+class GTFParser(object):
+    filename = ""
+    genes = {}
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.genes = {}
+
+    def fixChrom(self, c):
+        if c.startswith("chr"):
+            return c
+        else:
+            return "chr" + c
+
+    def getName(self, annots):
+        parts = annots.split(";")
+        for p in parts:
+            p = p.lstrip()
+            if p.startswith("gene_name"):
+                return p[10:].strip('"')
+        return None
+
+    def load(self):
+        sys.stderr.write("Loading genes from GTF file {}...".format(self.filename))
+        with open(self.filename, "r") as f:
+            c = csv.reader(f, delimiter='\t')
+            for line in c:
+                if line[0][0] == '#':
+                    continue
+                if line[2] != "gene":
+                    continue
+                chrom = self.fixChrom(line[0])
+                start = int(line[3])
+                end = int(line[4])
+                strand = line[6]
+                name = self.getName(line[8])
+                if name:
+                    self.genes[name] = [chrom, start, end, strand]
+        sys.stderr.write(" done, {} genes loaded.\n".format(len(self.genes)))
+
+    def get(self, name):
+        return self.genes[name] if name in self.genes else None
+
+# And we get to also parse GFF almost for free...
+
+class GFFParser(GTFParser):
+
+    def getName(self, annots):
+        parts = annots.split(";")
+        for p in parts:
+            p = p.lstrip()
+            if p.startswith("Name="):
+                return p[5:]
+        return None
+
+## Main
+
+class Main(object):
+    genelist = None
+    genes = []
+    outfile = "/dev/stdout"
+    reference = ""
+    seqman = None
+    upstream = 400
+    downstream = 100
+    field = 2000
+    toFasta = False
+
+    def __init__(self):
+        self.genes = []
+
+    def parseArgs(self, args):
+        if "-h" in args or "--help" in args:
+            return self.usage()
+        prev = ""
+        for a in args:
+            if prev == "-o":
+                self.outfile = a
+                prev = ""
+            elif prev == "-r":
+                self.reference = a
+                prev = ""
+            elif prev == "-u":
+                self.upstream = int(a)
+                prev = ""
+            elif prev == "-u":
+                self.upstream = int(a)
+                prev = ""
+            elif prev == "-d":
+                self.downstream = int(a)
+                prev = ""
+            elif prev == "-w":
+                self.field = int(a)
+                prev = ""
+            elif a in ["-o", "-r", "-u", "-d", "-w"]:
+                prev = a
+            elif a == "-f":
+                self.toFasta = True
+            elif self.genelist is None:
+                ext = os.path.splitext(a)[1]
+                if ext in [".gtf", ".GTF"]:
+                    self.genelist = GTFParser(a)
+                elif ext in [".gff", ".gff3", ".GFF", ".GFF3"]:
+                    self.genelist = GFFParser(a)
+                else:
+                    sys.stderr.write("Error: gene database is not a GTF or GFF file.\n")
+                    return False
+            else:
+                if a[0] == '@':
+                    with open(a[1:], "r") as f:
+                        self.genes += [ g.strip() for g in f.read().split("\n") ]
+                else:
+                    self.genes.append(a)
+        if os.path.isfile(self.reference):
+            self.seqman = SequenceManager(self.reference)
+            if self.genelist and self.genes:
+                self.genelist.load()
+                return True
+            
+            else:
+                return self.usage()
+        else:
+            sys.stderr.write("Error: please specify genome reference file with -r option.\n")
+            return False
+
+    def usage(self):
+        sys.stdout.write("""fengc.py - design primers for FENGC experiments.
+
+Usage: fengc.py [options] genesdb genes...
+
+where `genesdb' is a gene database in GFT or GFF format, and `genes' is one or
+more gene names, or (if preceded by @) a file containing gene names, one per line.
+
+Options:
+
+  -r R | Specify location of genome reference file (required).
+  -o O | Write output to file O (default: standard output).
+  -f   | Write target sequences to FASTA files.
+
+  -u U | Number of bp upstream of TSS (default: {}).
+  -d D | Number of bp downstream of TSS (default: {}).
+  -w W | Number of bp upstream/downstream of regions of interest (default: {}).
+
+""".format(self.upstream, self.downstream, self.field))
+        return False
+
+    def run(self):
+        gnames = []
+        gcoords = []
+
+        for g in self.genes:
+            coords = self.genelist.get(g)
+            if coords:
+                gnames.append(g)
+                if coords[3] == '+':
+                    gcoords.append([coords[0], coords[1] - self.upstream - self.field, coords[1] + self.downstream + self.field, coords[3]])
+                else:
+                    gcoords.append([coords[0], coords[2] - self.downstream - self.field, coords[2] + self.upstream + self.field, coords[3]])
+                sys.stderr.write("{:30} {}:{}-{}:{}\n".format(g, coords[0], coords[1], coords[2], coords[3]))
+
+        seqs = self.seqman.getSequences(gcoords)
+        for i in range(len(gnames)):
+            seq = seqs[i]
+            if gcoords[i][3] == '-':
+                ## TODO: reverse-complement sequence
+                pass
+            seq.name = gnames[i] + " " + seq.name[1:]
+            if self.toFasta:
+                with open(gnames[i] + ".fa", "w") as out:
+                    out.write(">{}{}".format(seq.name, seq.seq))
+
+### Let's get things started
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    M = Main()
+    if M.parseArgs(args):
+        M.run()
+    else:
+        M.usage()
+
+### Test with:
+
+## ./fengc.py -f -r /ufrc/data/reference/icbr/GRCh38/GRCh38.fa /ufrc/data/reference/icbr/GRCh38/Homo_sapiens.GRCh38.95.gtf APOE KDM6A TLR4
